451a73c53b01e5f64d1d9bb15eb943e793150314
Lxxyx <Lxxyxzj@gmail.com>
2017-08-01 22:23:04 +0800

    move add host to httpUtils

--- js/network/Urls.js
+++ js/network/Urls.js
@@ -1,25 +1,19 @@
-let HOST = 'https://1ibrary.group/'
-
-export const setHost = (host) => {
-  HOST = host
-}
-
 export const USERS = {
-  login: HOST + 'users/login',
-  feedback: HOST + 'users/feedback'
+  login: 'users/login',
+  feedback: 'users/feedback'
 }
 
 export const LISTS = {
-  create_list: HOST + 'lists/create_list',
-  show_list: HOST + 'lists/show_list',
-  show_detail: HOST + 'lists/show_detail',
-  collect_book: HOST + 'lists/collect_book',
-  update_list: HOST + 'lists/update_list',
-  remove_list: HOST + 'lists/remove_list'
+  create_list: 'lists/create_list',
+  show_list: 'lists/show_list',
+  show_detail: 'lists/show_detail',
+  collect_book: 'lists/collect_book',
+  update_list: 'lists/update_list',
+  remove_list: 'lists/remove_list'
 }
 
 export const BOOKS = {
-  show_detail: HOST + 'books/show_detail',
-  show_books: HOST + 'books/show_books',
-  search_book: HOST + 'books/search_book'
+  show_detail: 'books/show_detail',
+  show_books: 'books/show_books',
+  search_book: 'books/search_book'
 }
