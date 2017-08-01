4e8e8635a0b930cc1d2c3c8869f7d6c3067d934c
Lxxyx <Lxxyxzj@gmail.com>
2017-08-01 22:09:20 +0800

    avoid console bugs

--- js/pages/AppRouter.js
+++ js/pages/AppRouter.js
@@ -112,3 +112,21 @@ export default class AppRouter extends Component {
 }
 
 console.disableYellowBox = true
+
+/**
+ * RN-BUGS
+ * 在Debug环境下console.dir有效，
+ * 生产环境下console.dir为undefined。所以需要打个补丁
+ * 以下补丁同理
+ */
+if (!(console.dir instanceof Function)) {
+  console.dir = console.log
+}
+
+if (!(console.time instanceof Function)) {
+  console.time = console.log
+}
+
+if (!(console.timeEnd instanceof Function)) {
+  console.timeEnd = console.log
+}
