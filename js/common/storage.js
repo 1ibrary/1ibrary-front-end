a6a2a3d8104c182d689d7f8391261ae2a91f3e8d
Lxxyx <Lxxyxzj@gmail.com>
2017-08-23 20:27:27 +0800

    fix(common): 修复拼写错误的bug

--- js/common/storage.js
+++ js/common/storage.js
@@ -4,6 +4,7 @@ export default class Storage {
   static async set(key, value) {
     await AsyncStorage.setItem(key, JSON.stringify(value))
   }
+
   static async get(key, defaultValue) {
     let result = await AsyncStorage.getItem(key)
     if (!result) {
@@ -13,7 +14,7 @@ export default class Storage {
       return JSON.parse(result)
     } catch (error) {
       console.log(error)
-      return defaulValue || undefined
+      return defaultValue || undefined
     }
   }
 }
